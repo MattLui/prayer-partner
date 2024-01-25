00b1391053b312d5ace9f7b553f810fa98d84091
Matt Lui <Mattclui@gmail.com>
2024-01-25 08:28:56 -0800

    pp

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ const requiresAuthentication = (req, res, next) => {
   }
 };
 
+// Redirect start page - pp
+app.get("/pp", (req, res) => {
+  res.redirect("/categories");
+});
+
 // Redirect start page
 app.get("/", (req, res) => {
   res.redirect("/categories");

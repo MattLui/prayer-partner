7ff71949d0f168d7a18453c33572aaf5f1be6bf3
Matt Lui <Mattclui@gmail.com>
2024-01-24 21:44:38 -0800

    config files

---
+++ lib/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password)
+  VALUES ('Matt', '$2b$10$Nfcbm9.3t.4gmzx1dfsdCub33OGs06N7kBe2lNA0IACRPdEcdVWEi'),
+         ('Victoria', '$2b$10$mUO55.s3eCMUC1xnyPCS4.DJmE06r8Jqx6dXlaL6s3kQuwlpQ59XS'),
+         ('Taylor', '$2b$10$8qXYB46cz243EfM4nV1Ud.XPS4uPfvl0FV6ggITE4nYZ69TnasvWS');
